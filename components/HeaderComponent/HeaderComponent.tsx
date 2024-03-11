a9fa7a88cba2c374c8ba7e61ff7cf1e831b61468
markusmaisch <markus.maisch@coliquio.de>
2024-03-11 09:18:48 +0100

    fix: image priority

--- components/HeaderComponent/HeaderComponent.tsx
+++ components/HeaderComponent/HeaderComponent.tsx
@@ -4,7 +4,7 @@ const HeaderComponent = () => {
     return (
         <div className="p-4 flex md:flex-row flex-col justify-between bg-gray-100 items-center">
             <h1 className="text-4xl font-bold">Next.JS Demo</h1>
-            <Image src="/seeIT.svg" alt="logo" width={200} height={200}/>
+            <Image priority src="/seeIT.svg" alt="logo" width={200} height={200}/>
         </div>
     )
 }
